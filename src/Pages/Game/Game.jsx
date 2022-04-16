2d13e5e6983db4b9f5b786ae13cecba4e09c0dea
rodas007 <sergioproba@gmail.com>
2022-04-16 17:43:19 +0200

    Fix Styles Buttons

--- src/Pages/Game/Game.jsx
+++ src/Pages/Game/Game.jsx
@@ -13,7 +13,13 @@ const images = {
 
 export default function Game() {
   const [counter, setCounter] = useState(0);
-  const [array, setArray] = useState([]);
+  const [array, setArray] = useState([0, 1, 2, 3]);
+  const [random, setRandom] = useState();
+  const setWinner = () => {
+    setRandom(Math.floor(Math.random() * tableBoard.length));
+  };
+  console.log("winner", random);
+
   const arrayFour = () => {
     setArray([0, 1, 2, 3]);
   };
@@ -24,9 +30,6 @@ export default function Game() {
     setArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
   };
   const tableBoard = array;
-  const random = Math.floor(Math.random() * tableBoard.length);
-  console.log(array);
-  console.log(tableBoard.length);
 
   return (
     <div className="app template">
@@ -40,35 +43,62 @@ export default function Game() {
           <img src={chest} alt="x"></img>
         </div>
       </div>
+      <div clasName="c-tamtablero">
+      <h2>Elige un tablero</h2>
+
+      <div className="c-buttonfour">
+          <button button class="button-33"
+            onClick={() => {
+              arrayFour();
+              setWinner();
+            }}
+          >
+           <span>TABLERO 4</span>
+          <div class="liquid"></div>
+          </button>
+          </div>
+          <div className="c-buttonfour">
+          <button button class="button-33"
+            onClick={() => {
+              arrayEight();
+              setWinner();
+            }}
+          >
+          <span>TABLERO 8</span>
+          <div class="liquid"></div>
+          </button>
+          </div>
+          <div className="c-buttonfour">
+          <button button class="button-33"
+            onClick={() => {
+              arraySixteen();
+              setWinner();
+            }}
+          >
+            
+            <span>TABLERO 16</span>
+          <div class="liquid"></div>
+          </button>
+        </div> 
+        </div>
 
       <div className="tablero">
         {tableBoard.map((index) => (
           <div key={index}>
-            <Tablero
-              carta={images}
-              index={index}
-              random={random}
-              setCounter={setCounter}
-              counter={counter}
-            />
+            <Tablero carta={images} index={index} random={random} setCounter={setCounter} counter={counter} />
           </div>
         ))}
       </div>
+      
 
       <div>
         <div className="marcador">
           <h2>INTENTOS</h2>
           <h4>{counter}</h4>
         </div>
-        <div>
-          <button onClick={() => arrayFour()}>TABLERO 4</button>
-        </div>
-        <div>
-          <button onClick={() => arrayEight()}>TABLERO 8</button>
-        </div>
-        <div>
-          <button onClick={() => arraySixteen()}>TABLERO 16</button>
-        </div>
+       
+        
+        
       </div>
     </div>
   );
